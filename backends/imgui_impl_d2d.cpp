36f83eb34f77740b8544e434e6f95bd28d19fe06
rymut <boguslaw@rymut.org>
2024-01-10 23:43:15 +0100

    refactor: simplify polygon detection

--- backends/imgui_impl_d2d.cpp
+++ backends/imgui_impl_d2d.cpp
@@ -47,6 +47,53 @@ struct ImGui_ImplD2D_Fonts {
 
 };
 
+// every figure have an offset
+enum ImPolygonSides_ {
+    // default none
+    // indicates: 0
+    ImPolygonSides_None,
+    // triangle - flat, line, grad2, grad3, texture
+    // indicates: 3
+    ImPolygonSides_Three,
+    // quad - flat, line, grad2, texture, glyph
+    // indicates: 6
+    ImPolygonSides_Four,
+    // more - flat, line?
+    // parameters: ImU32 (count)
+    ImPolygonSides_Many,
+};
+
+enum ImPolygonShading_ {
+    ImPolygonShading_None,
+    // line - used base texture triangle
+    // parametres: line 0-64 & color (ImU32)
+    ImPolygonShading_Line,
+    // single color
+    // parameters: ImU32
+    ImPolygonShading_Flat,
+    // gradient of 2 colors
+    // parameters: position and color A (ImVec2, ImU32)
+    // parameters: position and color B (ImVec2, ImU32)
+    ImPolygonShading_Gradient2,
+    // gradient of 3 colors
+    // parameters stored in verticles
+    // parameters: position and color A (ImVec2, ImU32)
+    // parameters: position and color B (ImVec2, ImU32)
+    // parameters: position and color C (ImVec2, ImU32)
+    ImPolygonShading_Gradient3,
+    // texture fill
+    // parameters: texture id (known for vertex)
+    // parameters: rotation (not supported now)
+    // parameters: scale
+    // compute: base point ImVec2 (on texture)
+    ImPolygonShading_Texture,
+    // font fill
+    // parameters: font id 
+    // parameters: ImWchar (glyph id)
+    // parameters: Scale (different than base)
+    ImPolygonShading_FontGlyph
+};
+
 struct ImGui_ImplD2D_Data
 {
     ID2D1Factory* Factory;
@@ -179,13 +226,271 @@ void     ImGui_ImplD2D_NewFrame() {
     // auto detect new fonts if required
     ImGui_ImplD2D_CreateFontsTexture();
 }
+static float ImGui_Impl2D2_ColorMap[256] = {
+    0.0f / 255.0f,
+    1.0f / 255.0f,
+    2.0f / 255.0f,
+    3.0f / 255.0f,
+    4.0f / 255.0f,
+    5.0f / 255.0f,
+    6.0f / 255.0f,
+    7.0f / 255.0f,
+    8.0f / 255.0f,
+    9.0f / 255.0f,
+    10.0f / 255.0f,
+    11.0f / 255.0f,
+    12.0f / 255.0f,
+    13.0f / 255.0f,
+    14.0f / 255.0f,
+    15.0f / 255.0f,
+    16.0f / 255.0f,
+    17.0f / 255.0f,
+    18.0f / 255.0f,
+    19.0f / 255.0f,
+    20.0f / 255.0f,
+    21.0f / 255.0f,
+    22.0f / 255.0f,
+    23.0f / 255.0f,
+    24.0f / 255.0f,
+    25.0f / 255.0f,
+    26.0f / 255.0f,
+    27.0f / 255.0f,
+    28.0f / 255.0f,
+    29.0f / 255.0f,
+    30.0f / 255.0f,
+    31.0f / 255.0f,
+    32.0f / 255.0f,
+    33.0f / 255.0f,
+    34.0f / 255.0f,
+    35.0f / 255.0f,
+    36.0f / 255.0f,
+    37.0f / 255.0f,
+    38.0f / 255.0f,
+    39.0f / 255.0f,
+    40.0f / 255.0f,
+    41.0f / 255.0f,
+    42.0f / 255.0f,
+    43.0f / 255.0f,
+    44.0f / 255.0f,
+    45.0f / 255.0f,
+    46.0f / 255.0f,
+    47.0f / 255.0f,
+    48.0f / 255.0f,
+    49.0f / 255.0f,
+    50.0f / 255.0f,
+    51.0f / 255.0f,
+    52.0f / 255.0f,
+    53.0f / 255.0f,
+    54.0f / 255.0f,
+    55.0f / 255.0f,
+    56.0f / 255.0f,
+    57.0f / 255.0f,
+    58.0f / 255.0f,
+    59.0f / 255.0f,
+    60.0f / 255.0f,
+    61.0f / 255.0f,
+    62.0f / 255.0f,
+    63.0f / 255.0f,
+    64.0f / 255.0f,
+    65.0f / 255.0f,
+    66.0f / 255.0f,
+    67.0f / 255.0f,
+    68.0f / 255.0f,
+    69.0f / 255.0f,
+    70.0f / 255.0f,
+    71.0f / 255.0f,
+    72.0f / 255.0f,
+    73.0f / 255.0f,
+    74.0f / 255.0f,
+    75.0f / 255.0f,
+    76.0f / 255.0f,
+    77.0f / 255.0f,
+    78.0f / 255.0f,
+    79.0f / 255.0f,
+    80.0f / 255.0f,
+    81.0f / 255.0f,
+    82.0f / 255.0f,
+    83.0f / 255.0f,
+    84.0f / 255.0f,
+    85.0f / 255.0f,
+    86.0f / 255.0f,
+    87.0f / 255.0f,
+    88.0f / 255.0f,
+    89.0f / 255.0f,
+    90.0f / 255.0f,
+    91.0f / 255.0f,
+    92.0f / 255.0f,
+    93.0f / 255.0f,
+    94.0f / 255.0f,
+    95.0f / 255.0f,
+    96.0f / 255.0f,
+    97.0f / 255.0f,
+    98.0f / 255.0f,
+    99.0f / 255.0f,
+    100.0f / 255.0f,
+    101.0f / 255.0f,
+    102.0f / 255.0f,
+    103.0f / 255.0f,
+    104.0f / 255.0f,
+    105.0f / 255.0f,
+    106.0f / 255.0f,
+    107.0f / 255.0f,
+    108.0f / 255.0f,
+    109.0f / 255.0f,
+    110.0f / 255.0f,
+    111.0f / 255.0f,
+    112.0f / 255.0f,
+    113.0f / 255.0f,
+    114.0f / 255.0f,
+    115.0f / 255.0f,
+    116.0f / 255.0f,
+    117.0f / 255.0f,
+    118.0f / 255.0f,
+    119.0f / 255.0f,
+    120.0f / 255.0f,
+    121.0f / 255.0f,
+    122.0f / 255.0f,
+    123.0f / 255.0f,
+    124.0f / 255.0f,
+    125.0f / 255.0f,
+    126.0f / 255.0f,
+    127.0f / 255.0f,
+    128.0f / 255.0f,
+    129.0f / 255.0f,
+    130.0f / 255.0f,
+    131.0f / 255.0f,
+    132.0f / 255.0f,
+    133.0f / 255.0f,
+    134.0f / 255.0f,
+    135.0f / 255.0f,
+    136.0f / 255.0f,
+    137.0f / 255.0f,
+    138.0f / 255.0f,
+    139.0f / 255.0f,
+    140.0f / 255.0f,
+    141.0f / 255.0f,
+    142.0f / 255.0f,
+    143.0f / 255.0f,
+    144.0f / 255.0f,
+    145.0f / 255.0f,
+    146.0f / 255.0f,
+    147.0f / 255.0f,
+    148.0f / 255.0f,
+    149.0f / 255.0f,
+    150.0f / 255.0f,
+    151.0f / 255.0f,
+    152.0f / 255.0f,
+    153.0f / 255.0f,
+    154.0f / 255.0f,
+    155.0f / 255.0f,
+    156.0f / 255.0f,
+    157.0f / 255.0f,
+    158.0f / 255.0f,
+    159.0f / 255.0f,
+    160.0f / 255.0f,
+    161.0f / 255.0f,
+    162.0f / 255.0f,
+    163.0f / 255.0f,
+    164.0f / 255.0f,
+    165.0f / 255.0f,
+    166.0f / 255.0f,
+    167.0f / 255.0f,
+    168.0f / 255.0f,
+    169.0f / 255.0f,
+    170.0f / 255.0f,
+    171.0f / 255.0f,
+    172.0f / 255.0f,
+    173.0f / 255.0f,
+    174.0f / 255.0f,
+    175.0f / 255.0f,
+    176.0f / 255.0f,
+    177.0f / 255.0f,
+    178.0f / 255.0f,
+    179.0f / 255.0f,
+    180.0f / 255.0f,
+    181.0f / 255.0f,
+    182.0f / 255.0f,
+    183.0f / 255.0f,
+    184.0f / 255.0f,
+    185.0f / 255.0f,
+    186.0f / 255.0f,
+    187.0f / 255.0f,
+    188.0f / 255.0f,
+    189.0f / 255.0f,
+    190.0f / 255.0f,
+    191.0f / 255.0f,
+    192.0f / 255.0f,
+    193.0f / 255.0f,
+    194.0f / 255.0f,
+    195.0f / 255.0f,
+    196.0f / 255.0f,
+    197.0f / 255.0f,
+    198.0f / 255.0f,
+    199.0f / 255.0f,
+    200.0f / 255.0f,
+    201.0f / 255.0f,
+    202.0f / 255.0f,
+    203.0f / 255.0f,
+    204.0f / 255.0f,
+    205.0f / 255.0f,
+    206.0f / 255.0f,
+    207.0f / 255.0f,
+    208.0f / 255.0f,
+    209.0f / 255.0f,
+    210.0f / 255.0f,
+    211.0f / 255.0f,
+    212.0f / 255.0f,
+    213.0f / 255.0f,
+    214.0f / 255.0f,
+    215.0f / 255.0f,
+    216.0f / 255.0f,
+    217.0f / 255.0f,
+    218.0f / 255.0f,
+    219.0f / 255.0f,
+    220.0f / 255.0f,
+    221.0f / 255.0f,
+    222.0f / 255.0f,
+    223.0f / 255.0f,
+    224.0f / 255.0f,
+    225.0f / 255.0f,
+    226.0f / 255.0f,
+    227.0f / 255.0f,
+    228.0f / 255.0f,
+    229.0f / 255.0f,
+    230.0f / 255.0f,
+    231.0f / 255.0f,
+    232.0f / 255.0f,
+    233.0f / 255.0f,
+    234.0f / 255.0f,
+    235.0f / 255.0f,
+    236.0f / 255.0f,
+    237.0f / 255.0f,
+    238.0f / 255.0f,
+    239.0f / 255.0f,
+    240.0f / 255.0f,
+    241.0f / 255.0f,
+    242.0f / 255.0f,
+    243.0f / 255.0f,
+    244.0f / 255.0f,
+    245.0f / 255.0f,
+    246.0f / 255.0f,
+    247.0f / 255.0f,
+    248.0f / 255.0f,
+    249.0f / 255.0f,
+    250.0f / 255.0f,
+    251.0f / 255.0f,
+    252.0f / 255.0f,
+    253.0f / 255.0f,
+    254.0f / 255.0f,
+    255.0f / 255.0f
+};
 
 inline static D2D1_COLOR_F ImGui_ImplD2D_Color(ImU32 color) {
     constexpr float norm = 1.0f / 255.0f;
-    const float a = (color >> 24 & 0xFFu) * norm;
-    const float b = ((color >> 16) & 0xFFu) * norm;
-    const float g = ((color >> 8) & 0xFFu) * norm;
-    const float r = ((color >> 0) & 0xFFu) * norm;
+    const float a = ImGui_Impl2D2_ColorMap[color >> 24 & 0xFFu];
+    const float b = ImGui_Impl2D2_ColorMap[(color >> 16) & 0xFFu];
+    const float g = ImGui_Impl2D2_ColorMap[(color >> 8) & 0xFFu];
+    const float r = ImGui_Impl2D2_ColorMap[(color >> 0) & 0xFFu];
     return D2D1_COLOR_F{ r, g, b, a };
 }
 
@@ -462,12 +767,86 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                 D2D1_RECT_F clip = D2D1::RectF(clip_min.x, clip_min.y, clip_max.x, clip_max.y);
                 bd->RenderTarget->PushAxisAlignedClip(clip, D2D1_ANTIALIAS_MODE_ALIASED);
 
-
-                int vertCount = cmd_list->VtxBuffer.Size - pcmd->VtxOffset;
-                int indCount = pcmd->ElemCount;
+                unsigned int indCount = pcmd->ElemCount;
+                if (indCount == 0)
+                {
+                    continue;
+                }
                 const ImDrawVert* vert = vtx_buffer + pcmd->VtxOffset;
                 const ImDrawIdx* idx = idx_buffer + pcmd->IdxOffset;
+                const ImTextureID texture = pcmd->GetTexID();
+                const bool isFontOrLine = texture == io.Fonts->TexID;
+                if (true)
+                {
+                    int idxOffset = 0;
+                    pathGeometry.Release();
+                    geometrySink.Release();
+                    while (idxOffset < indCount) {
+                        int polygonIndicates = 0;
+                        ImDrawIdx prevIdx[3] = { idx[idxOffset + 0], idx[idxOffset + 1], idx[idxOffset + 2] };
+                        ImU32 prevCol = (vert + prevIdx[0])->col;
+                        for (int i = idxOffset; i < indCount; i += 3) {
+                            ImDrawIdx currIdx[3] = { idx[i], idx[i + 1], idx[i + 2] };
+                            const bool commonIndicateTest =
+                                prevIdx[0] == currIdx[0] || prevIdx[0] == currIdx[1] || prevIdx[0] == currIdx[2] ||
+                                prevIdx[1] == currIdx[0] || prevIdx[1] == currIdx[1] || prevIdx[1] == currIdx[2] ||
+                                prevIdx[2] == currIdx[0] || prevIdx[2] == currIdx[1] || prevIdx[2] == currIdx[2];
+                            if (commonIndicateTest == false) {
+                                break;
+                            }
+                            const ImDrawVert* verts[3] = { vert + currIdx[0], vert + currIdx[1], vert + currIdx[2] };
+                            const bool sameColorTest = prevCol == verts[0]->col || prevCol == verts[1]->col || prevCol == verts[2]->col;
+                            if (sameColorTest == false) {
+                                break;
+                            }
+                            memcpy(&prevIdx, &currIdx, sizeof(currIdx));
+                            polygonIndicates += 3;
+                        }
+                        const int idxStart = idxOffset;
+                        idxOffset += polygonIndicates;
+                        // drawing
+                        hr = bd->Factory->CreatePathGeometry(&pathGeometry.Pointer);
+                        if (FAILED(hr))
+                        {
+                            continue;
+                        }
+                        hr = pathGeometry.Pointer->Open(&geometrySink.Pointer);
+                        if (FAILED(hr))
+                        {
+                            continue;
+                        }
+                        geometrySink.Pointer->SetFillMode(D2D1_FILL_MODE_ALTERNATE);
+                        geometrySink.Pointer->SetSegmentFlags(D2D1_PATH_SEGMENT_FORCE_ROUND_LINE_JOIN);
+                        D2D1_POINT_2F point;
+                        for (int i = idxStart; i < idxOffset; i += 3) {
+                            int o = idx[i];
+                            point.x = (vert + o)->pos.x;
+                            point.y = (vert + o)->pos.y;
+                            geometrySink.Pointer->BeginFigure(point, D2D1_FIGURE_BEGIN_FILLED);
+                            geometrySink.Pointer->AddLine(point);
+                            o = idx[i + 1];
+                            point.x = (vert + o)->pos.x;
+                            point.y = (vert + o)->pos.y;
+                            geometrySink.Pointer->AddLine(point);
+                            o = idx[i + 2];
+                            point.x = (vert + o)->pos.x;
+                            point.y = (vert + o)->pos.y;
+                            geometrySink.Pointer->AddLine(point);
+                            geometrySink.Pointer->EndFigure(D2D1_FIGURE_END_CLOSED);
+                        }
+                        hr = geometrySink.Pointer->Close();
+                        geometrySink.Release();
+                        if (FAILED(hr))
+                        {
+                            continue;
+                        }
 
+                        bd->SolidColorBrush.Pointer->SetColor(ImGui_ImplD2D_Color(prevCol));
+                        bd->RenderTarget->FillGeometry(pathGeometry.Pointer, bd->SolidColorBrush.Pointer);
+                        pathGeometry.Release();
+                    }
+                }
+                else
                 {
                     pathGeometry.Release();
                     hr = bd->Factory->CreatePathGeometry(&pathGeometry.Pointer);
@@ -482,15 +861,19 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                         continue;
                     }
 
-                    ImTextureID texture = pcmd->GetTexID();
-                    const bool isFont = texture = io.Fonts->TexID;
+                    const ImTextureID texture = pcmd->GetTexID();
+                    const bool isFont = texture == io.Fonts->TexID;
                     const ImDrawVert* xyFirst = vert + idx[0];
                     ImU32 color = xyFirst->col;
                     ImDrawIdx index = idx[2];
                     int figureIndCount = 0;
                     int figureIndStart = 0;
                     ImU32 green = ImGui::GetColorU32(IM_COL32(0, 255, 0, 255));
+
+
                     for (int i = 0; i < indCount; i += 3) {
+                        // 1 for every triangle
+                        // 1. checki
                         const ImDrawVert* xy0 = vert + idx[i];
                         const ImDrawVert* xy1 = vert + idx[i + 1];
                         const ImDrawVert* xy2 = vert + idx[i + 2];
@@ -526,10 +909,13 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                                     verts[2] = vert + idx[ioffset + 2];
                                     verts[3] = vert + idx[ioffset + figureIndCount - 1];
                                     if (verts[0]->col == verts[1]->col && verts[2]->col == verts[3]->col && verts[1]->col == verts[2]->col) {
+                                        // ImPolygonSides_Four || ImPolygonSides_three
                                         bd->SolidColorBrush.Pointer->SetColor(ImGui_ImplD2D_Color(color));
                                         bd->RenderTarget->FillGeometry(pathGeometry.Pointer, bd->SolidColorBrush.Pointer);
                                     }
                                     else if (figureIndCount == 6 && (verts[0]->col == verts[3]->col || verts[0]->col == verts[1]->col)) {
+                                        // ImPolygonSides_Four
+                                        // ImPolygonShading_Gradient2
                                         bool success = true;
                                         if (verts[0]->col == verts[3]->col) {
                                             success = ImGui_ImplD2D_CreateBrush(linGradBrush, bd->GradientStops, stopsCol, linGradProps, bd->RenderTarget,
@@ -549,6 +935,8 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                                         }
                                     }
                                     else {
+                                        // ImPolygonSides_Three
+                                        // ImPolygonShading_Gradient3
                                         ImVec2 middle;
                                         middle.x = 0.25 * (verts[0]->pos.x + verts[1]->pos.x + verts[2]->pos.x + verts[3]->pos.x);
                                         middle.y = 0.25 * (verts[0]->pos.y + verts[1]->pos.y + verts[2]->pos.y + verts[3]->pos.y);
@@ -574,6 +962,8 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                                     }
                                 }
                                 else {
+                                    // ImPolygonSides_Many
+                                    // ImPolygonShading_Flat
                                     bd->SolidColorBrush.Pointer->SetColor(ImGui_ImplD2D_Color(color));
                                     bd->RenderTarget->FillGeometry(pathGeometry.Pointer, bd->SolidColorBrush.Pointer);
                                 }
@@ -614,7 +1004,7 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                                 bd->SolidColorBrush.Pointer->SetColor(D2D1::ColorF(D2D1::ColorF::White));
                                 bd->RenderTarget->FillGeometry(pathGeometry.Pointer, bd->SolidColorBrush.Pointer);
 #endif // 0
-                            }
+                                }
                             xyFirst = xy0;
                             figureIndCount = 0;
                             figureIndStart = i;
@@ -630,7 +1020,7 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                             {
                                 continue;
                             }
-                        }
+                            }
                         int skip = ImGui_ImplD2D_IsGlyph(bd->RenderTarget, bd, io, pcmd, vert, idx, i);
                         if (skip > 0) {
                             // print stream
@@ -647,7 +1037,7 @@ void     ImGui_ImplD2D_RenderDrawData(ImDrawData* draw_data) {
                         figureIndCount += 3;
 
                         ImTextureID texture = pcmd->GetTexID();
-                    }
+                        }
                     hr = geometrySink.Pointer->Close();
                     if (FAILED(hr))
                     {
